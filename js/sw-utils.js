b7f8928500845426044371590e099a6020b54b53
mario56530 <mario56530>
2022-11-20 00:29:16 -0600

    primer commit

---
+++ js/sw-utils.js
@@ -0,0 +1,13 @@
+//guardar en el cache dinamico
+function actualizacacheDinamico(dynamicCache, req, res)
+{
+    if(res.ok){
+        return caches.open(dynamicCache).then(cache =>{
+            cache.put(req, res.clone());
+            return res.clone();
+        });
+    }else{
+        return res;
+    }
+    
+}
